f767ecef16c5f59d0de176d3729789b51a821b73
Pradana21-Bits <rezapradana9d@gmail.com>
2021-09-04 14:20:19 +0700

    add Sesi 3 And Read me

---
+++ session3/calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add(*args):
+    res = 0
+    for arg in args:
+        res += arg
+    return res
+def multiply(*args):
+    res = 1
+    for arg in args:
+        res = res * arg
+    return res
+def devide(*args):
+    [res, *sisa ] = args
+    for arg in args:
+        res =res / arg
+    return res
+
