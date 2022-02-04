5e54941cfbf82bfa17e88a6830255ff5490d20bf
Pradana21-Bits <58035968+Pradana21-Bits@users.noreply.github.com>
2022-02-04 17:01:09 +0700

    Add files via upload
    sesi 9 - 15

---
+++ session15/Flask/main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def alive():
+	return 'alive'
+
+@app.route('/test')
+def test():
+	myDict = {
+	"name " : "reza"
+	}
+	return myDict
+
+@app.route('/hello/<name>')
+def hello(name):
+	
+
+	return render_template("hello.html", name = name)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
